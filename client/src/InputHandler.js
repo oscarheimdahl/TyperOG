13b709f06705fab00a1687a0c95172f1c2c2abd7
oscarheimdahl <o.heimdahl@gmail.com>
2019-04-26 21:05:33 +0200

    Added progressbar with questionable style to inputhandler.

--- client/src/InputHandler.js
+++ client/src/InputHandler.js
@@ -11,8 +11,19 @@ export class InputHandler extends Component {
 		inputText: '',
 		completedText: [],
 		remainingText: this.props.text.split(' '),
-		complete: false
+		complete: false,
+		time: 0,
+		timerStarted: false
 	};
+
+	tick() {
+		setTimeout(() => {
+			if (!this.state.complete) {
+				this.setState({ time: this.state.time + 1 });
+				this.tick();
+			}
+		}, 1000);
+	}
 
 	constructor(props) {
 		super(props);
@@ -20,6 +31,10 @@ export class InputHandler extends Component {
 	}
 
 	handleInput = input => {
+		if (!this.state.timerStarted) {
+			this.tick();
+			this.setState({ timerStarted: true });
+		}
 		this.setState({ inputText: input });
 		let currentWord = this.state.words[this.state.wordIndex];
 		let correctString = currentWord.substring(0, input.length);
@@ -34,7 +49,7 @@ export class InputHandler extends Component {
 				inputText: '',
 				wordIndex: this.state.wordIndex + 1
 			});
-			this.setTextWord(input);
+			this.setTextWord(input, onLastWord);
 			if (onLastWord) this.setState({ complete: true });
 		} else if (correctString === input) {
 			this.setState({
@@ -60,53 +75,67 @@ export class InputHandler extends Component {
 	/**
 	 * Works similarly to 'setTextLetter' but a little different as a word is completed.
 	 */
-	setTextWord(input) {
+	setTextWord(input, onLastWord) {
 		this.state.completedText.pop();
 		this.state.completedText.push(input);
-		this.state.completedText.push('');
+		if (!onLastWord) this.state.completedText.push('');
 		this.state.remainingText.shift();
 	}
 
 	render() {
 		let temp = [...this.state.completedText];
+		let progress =
+			this.state.completedText.toString().length /
+			(this.props.text.length + this.state.words.length - 1);
+
 		temp.pop();
 		return (
-			<div className="container">
-				<input
-					value={this.state.inputText}
-					type="text"
-					placeholder="..."
-					className="inputfield"
-					style={
-						this.state.spelling === true
-							? {
-									background: '#DDFFDD'
-							  }
-							: {
-									background: 'pink'
-							  }
-					}
-					onChange={evt => {
-						if (!this.state.complete) {
-							this.handleInput(evt.target.value);
-						}
+			<div>
+				<div
+					className="progressbar"
+					style={{
+						width: progress * 100 + 'vw',
+						background: 'rgba(128, 128, 128,' + progress + ')'
 					}}
 				/>
-				<br />
-				<div
-					className="text"
-					style={this.state.complete ? { background: 'lightgreen' } : {}}
-				>
-					<span style={{ color: 'green' }}>{temp}</span>
-					<span
-						style={{
-							color: 'green',
-							textDecoration: 'underline'
+				<div className="container">
+					<input
+						value={this.state.inputText}
+						type="text"
+						placeholder="..."
+						className="inputfield"
+						style={
+							this.state.spelling === true
+								? {
+										background: '#DDFFDD'
+								  }
+								: {
+										background: 'pink'
+								  }
+						}
+						onChange={evt => {
+							if (!this.state.complete) {
+								this.handleInput(evt.target.value);
+							}
 						}}
+					/>
+					<br />
+					<div
+						className="text"
+						style={this.state.complete ? { background: 'lightgreen' } : {}}
 					>
-						{this.state.completedText[this.state.completedText.length - 1]}
-					</span>
-					<span>{this.state.remainingText.map(word => word + ' ')}</span>
+						<span style={{ color: 'green' }}>{temp}</span>
+						<span
+							style={{
+								color: 'green',
+								textDecoration: 'underline'
+							}}
+						>
+							{this.state.completedText[this.state.completedText.length - 1]}
+						</span>
+						<span>{this.state.remainingText.map(word => word + ' ')}</span>
+					</div>
+					Time: {this.state.time}
 				</div>
 			</div>
 		);
--- client/src/Play.js
+++ client/src/Play.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import InputHandler from './InputHandler';
 
-function App() {
+export default function App() {
 	let text =
 		'The Luger has a toggle-lock action which uses a jointed arm to lock, ' +
 		'as opposed to the slide actions of many other semi-automatic pistols.' +
 		' After a round is fired, the barrel and toggle assembly travel roughly ' +
 		'13 mm (0.5 in) rearward due to recoil, both locked together at this point.';
+	let text2 = 'This text is intentionally kind of short.';
 	return (
 		<div>
-			<InputHandler text={text} />
+			<InputHandler text={text2} />
 		</div>
 	);
 }
-
-export default App;
